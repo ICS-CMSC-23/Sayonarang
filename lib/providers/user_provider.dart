ca4745a7d2632b0a66aa200a08b9bd500b438094
rnlynmdrd <ramadrid1@up.edu.ph>
2024-05-21 23:44:50 +0800

    feat: implement uploading proof to firebase storage

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
 import '../api/firebase_user_api.dart';
+import 'dart:io';
 
 class MyAuthProvider with ChangeNotifier {
   late FirebaseAuthAPI authService;
@@ -46,5 +49,38 @@ class MyAuthProvider with ChangeNotifier {
   Future<void> signOut() async {
     await authService.signOut();
     notifyListeners();
+  }
+
+  Future<File?> getImageFromGallery(BuildContext context) async {
+    try {
+      final returnImage =
+          await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (returnImage == null) return null;
+      // print(returnImage.path);
+      return File(returnImage.path);
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<String> uploadFile(File file) async {
+    try {
+      // get reference to firebase storage
+      final storageRef = FirebaseStorage.instance.ref();
+
+      // get filename of the image
+      final fileName = file.path.split("/").last;
+      final timestamp = DateTime.now().microsecondsSinceEpoch;
+
+      // define the path in the storage
+      final uploadRef = storageRef.child("proof/$timestamp-$fileName");
+      await uploadRef.putFile(file);
+      print('Successfully uploaded file');
+      return '$timestamp-$fileName';
+    } catch (e) {
+      print(e);
+      return 'error';
+    }
   }
 }
