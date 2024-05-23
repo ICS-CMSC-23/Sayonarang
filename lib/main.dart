16cc51bb158ff2a096d36b7ef0b5e6e37770bd68
Francheska Marie Silapan <159750885+silapan@users.noreply.github.com>
2024-05-23 18:59:38 +0800

    Updated main imports

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:donation_app/screens/auth/login_page.dart';
 import 'package:donation_app/screens/auth/signup_page.dart';
 import 'package:donation_app/screens/org/org_main.dart';
 import 'package:donation_app/screens/admin/admin_home.dart';
-import 'package:donation_app/screens/donor/donor_main.dart';
+import 'package:donation_app/screens/donor/donor_home.dart';
 import 'package:donation_app/providers/donor_provider.dart';
 
 void main() async {
