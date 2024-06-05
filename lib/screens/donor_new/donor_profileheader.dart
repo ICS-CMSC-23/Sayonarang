281991b5417fb12486cb0c2864a292997fdb005d
Francheska Marie Silapan <159750885+silapan@users.noreply.github.com>
2024-06-06 07:49:19 +0800

    feat: fix dimensions for donor profile

--- lib/screens/donor_new/donor_profileheader.dart
+++ lib/screens/donor_new/donor_profileheader.dart
@@ -19,103 +19,97 @@ class ProfileHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: Dimensions.containerWidth,
-      height: Dimensions.containerHeight,
+      padding: const EdgeInsets.all(Dimensions.padding),
       decoration: BoxDecoration(
         color: Theme.of(context).cardColor,
+        borderRadius: BorderRadius.circular(16.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(Dimensions.padding),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                const CircleAvatar(
-                  radius: 60,
-                  backgroundColor: Color.fromARGB(255, 245, 243, 243),
-                  child: Icon(
-                    Icons.person,
-                    size: 60,
-                    color: Color.fromRGBO(245, 71, 65, 1),
-                  ),
-                ),
-                const SizedBox(width: 20),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        donorName,
-                        style: const TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.redAccent,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      const Text(
-                        'Username:',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      const SizedBox(height: 4),
-                      Text(
-                        username,
-                        style: const TextStyle(
-                          fontSize: 18,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const CircleAvatar(
+            radius: 60,
+            backgroundColor: Color.fromARGB(255, 245, 243, 243),
+            child: Icon(
+              Icons.person,
+              size: 60,
+              color: Color.fromRGBO(245, 71, 65, 1),
             ),
-            const SizedBox(height: 20),
-            const Text(
-              'Contact Number:',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey,
-              ),
+          ),
+          const SizedBox(height: Dimensions.spacing * 2),
+          Text(
+            donorName,
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.redAccent,
             ),
-            const SizedBox(height: 4),
-            Text(
-              contactNum,
+          ),
+          const SizedBox(height: Dimensions.spacing),
+          const Text(
+            'Username:',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey,
+            ),
+          ),
+          const SizedBox(height: Dimensions.spacing / 2),
+          Text(
+            username,
+            style: const TextStyle(
+              fontSize: 18,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(height: Dimensions.spacing * 2),
+          const Text(
+            'Contact Number:',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey,
+            ),
+          ),
+          const SizedBox(height: Dimensions.spacing / 2),
+          Text(
+            contactNum,
+            style: const TextStyle(
+              fontSize: 18,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(height: Dimensions.spacing * 2),
+          const Text(
+            'Addresses:',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey,
+            ),
+          ),
+          const SizedBox(height: Dimensions.spacing / 2),
+          ...addresses.map((address) => Padding(
+            padding: const EdgeInsets.only(bottom: Dimensions.spacing),
+            child: Text(
+              address,
               style: const TextStyle(
                 fontSize: 18,
                 color: Colors.black,
               ),
+              softWrap: true,
+              overflow: TextOverflow.visible,
             ),
-            const SizedBox(height: 20),
-            const Text(
-              'Addresses:',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey,
-              ),
-            ),
-            const SizedBox(height: 4),
-            ...addresses.map((address) => Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: Text(
-                address,
-                style: const TextStyle(
-                  fontSize: 18,
-                  color: Colors.black,
-                ),
-                softWrap: true,
-                overflow: TextOverflow.visible,
-              ),
-            )).toList(),
-          ],
-        ),
+          )).toList(),
+        ],
       ),
     );
   }
