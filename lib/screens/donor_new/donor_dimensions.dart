61cb039c5bfe60ced385d4b7be2cd6434663c0f6
Francheska Marie Silapan <159750885+silapan@users.noreply.github.com>
2024-06-06 07:48:13 +0800

    feat: fix dimensions for profile

--- lib/screens/donor_new/donor_dimensions.dart
+++ lib/screens/donor_new/donor_dimensions.dart
@@ -1,5 +1,6 @@
 class Dimensions {
-  static const double containerWidth = 400;
-  static const double containerHeight = 400.0;
-  static const double padding = 20;
+  static const double containerWidth = 400.0; 
+  static const double containerHeight = 500.0; 
+  static const double padding = 15.0;
+  static const double spacing = 10.0;
 }
